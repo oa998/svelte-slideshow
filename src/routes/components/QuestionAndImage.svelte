ec4d8b5342d0e0f74dc3aae05dad15a9a122caf5
zzz sss <onlineaccounts998@gmail.com>
2023-07-16 10:15:24 -0400

    auto commit

---
+++ src/routes/components/QuestionAndImage.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Center from '../components/Center.svelte';
+	import TriviaHalfSlide from '../components/TriviaHalfSlide.svelte';
+	import Unveil from '../components/Unveil.svelte';
+
+	let revealed = false;
+	let hint = false;
+	export let blanks1: string;
+	export let blanks2: string;
+	export let clue: string;
+	export let answer: string;
+	export let img: string;
+</script>
+
+<TriviaHalfSlide>
+	<Center class="rounded-xl flex flex-col gap-20" slot="left">
+		<div class="text-5xl px-10 question-shadow">{clue}</div>
+		{#if revealed}
+			<div class="text-5xl px-10 answer-shadow text-yellow-200 font-mon">{answer}</div>
+		{:else if hint}
+			<div class="text-5xl px-10 question-shadow font-mon">{blanks2}</div>
+		{:else}
+			<div class="text-5xl px-10 question-shadow font-mon">{blanks1}</div>
+		{/if}
+	</Center>
+	<Center class="rounded-xl overflow-hidden border border-white relative" slot="right">
+		<Unveil on:reveal={() => (revealed = true)} on:hint={() => (hint = true)} {img} />
+	</Center>
+</TriviaHalfSlide>
+
+<style>
+	.question-shadow {
+		text-shadow: #f33 0 0 5px, #f33 0 0 10px, #f33 0 0 20px, #f33 0 0 30px, #f33 0 0 40px,
+			#f33 0 0 60px, #f33 0 0 10px, #f33 0 0 10px;
+	}
+	.answer-shadow {
+		text-shadow: rgb(51, 51, 255) 0 0 5px, rgb(51, 51, 255) 0 0 10px, rgb(51, 51, 255) 0 0 20px,
+			rgb(51, 51, 255) 0 0 30px, rgb(51, 51, 255) 0 0 40px, rgb(51, 51, 255) 0 0 60px,
+			rgb(51, 51, 255) 0 0 10px, rgb(51, 51, 255) 0 0 10px;
+	}
+</style>
---
+++ src/routes/components/Unveil.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { createEventDispatcher } from 'svelte';
+
+	export let img: string;
+	let started = false;
+	let finished = false;
+	const dispatch = createEventDispatcher();
+	let can: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D;
+	const color = 240;
+	const startBlur = 50;
+	const startBrightness = 0.2;
+	let percentProgress = 0;
+
+	const setBlur = (blur: number, brightness: number) => {
+		if (browser) {
+			console.log({ blur, brightness });
+			document.documentElement.style.setProperty('--reveal-blur', `${blur}px`);
+			document.documentElement.style.setProperty(
+				'--reveal-brightness',
+				`${Math.min(1, brightness)}`
+			);
+		}
+	};
+	setBlur(startBlur, startBrightness);
+
+	const progress = (v: number) => {
+		percentProgress = 1 - v / 100;
+		setBlur(startBlur * percentProgress, 1 - percentProgress + startBrightness);
+		if (percentProgress < 0) dispatch('reveal');
+		else if (percentProgress < 0.4) dispatch('hint');
+	};
+
+	let reveal = () => {};
+
+	$: if (can) {
+		ctx = can.getContext('2d')!;
+
+		let end = Math.PI;
+		const start = end;
+		const d = can.height / 2.2;
+		const delta = 0.005;
+
+		reveal = () => {
+			finished = true;
+			progress(100.1);
+			// dispatch('reveal');
+		};
+
+		const draw = () => {
+			ctx.strokeStyle = `hsla(${Math.floor(color - (100 - 100 * percentProgress))}, 100%, 45%, 1)`;
+			ctx.save();
+			ctx.translate(can.width / 2, can.height / 2);
+			ctx.beginPath();
+			ctx.moveTo(0, 0);
+			ctx.rotate(end);
+			ctx.lineTo(0, d);
+			ctx.closePath();
+			ctx.stroke();
+			ctx.restore();
+			end += delta;
+			if (end - start < Math.PI * 2 + delta && !finished) {
+				progress(((end - start) / (Math.PI * 2)) * 100);
+				requestAnimationFrame(draw);
+			}
+		};
+		if (!started) {
+			started = true;
+			draw();
+		}
+	}
+
+	if (browser) {
+		document.addEventListener('keydown', (e) => {
+			if (e.key === ' ') {
+				reveal();
+			}
+		});
+	}
+</script>
+
+<img class="w-full h-auto blur" src={img} alt="ok" />
+<canvas width="150" height="150" bind:this={can} />
+
+<style>
+	:root {
+		--reveal-blur: 50px;
+		--reveal-brightness: 0;
+	}
+	.blur {
+		filter: blur(var(--reveal-blur)) brightness(var(--reveal-brightness));
+	}
+	canvas {
+		@apply absolute top-2 left-2 rounded-full bg-black;
+	}
+</style>
