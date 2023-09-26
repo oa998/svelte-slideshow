eed2c778d05dff23f18773901de95b64d1807d05
zzz sss <onlineaccounts998@gmail.com>
2023-09-25 21:22:46 -0400

    auto commit

--- src/routes/trivia/jeopardy/[number]/+page.server.ts
+++ src/routes/trivia/jeopardy/[number]/+page.server.ts
@@ -1,41 +1,42 @@
+import { base } from '$app/paths';
 import { redirect } from '@sveltejs/kit';
 
 const questions = [
 	{
-		imgurl: '/jeopardy/fj.png'
+		imgurl: `/${base}/jeopardy/fj.png`
 	},
 	{
 		category: 'The Silver Screen',
 		question: `He was the first actor to star in 3 films that won the Oscar for Best Picture: those of 1934, 1935 & 1939`,
 		answer: 'Who is Clark Gable?',
-		imgurl: '/jeopardy/cg.jpg'
+		imgurl: `/${base}/jeopardy/cg.jpg`
 	},
 	{
 		category: 'DISNEY CHARACTERS',
 		question: `In the source material from more than 3 centuries ago, her name was Badr al-Budur, “full moon of full moons”`,
 		answer: 'Who is Princess Jasmine?',
-		imgurl: '/jeopardy/aladdin.jpg'
+		imgurl: `/${base}/jeopardy/aladdin.jpg`
 	},
 	{
 		category: '19th CENTURY AMERICANS',
 		question: `Demonstrating the dignity & humanity of black Americans, he sat for 160 known photographs, the most of any American in the 19th century`,
 		answer: 'Who is FREDERICK DOUGLASS?',
-		imgurl: '/jeopardy/fd.jpg'
+		imgurl: `/${base}/jeopardy/fd.jpg`
 	},
 	{
 		category: 'TECH HISTORY',
 		question: `For about 20 years after its invention, it had few practical uses; then suddenly it revolutionized grocery stores & home audio`,
 		answer: 'What is the laser?',
-		imgurl: '/jeopardy/800.png'
+		imgurl: `/${base}/jeopardy/800.png`
 	},
 	{
 		category: 'TELEVISION HISTORY',
 		question: `In the opening scene of its July 21, 1969 pilot episode, a man carves the letter D into wet cement`,
 		answer: 'What is SESAME STREET?',
-		imgurl: '/jeopardy/ss.jpg'
+		imgurl: `/${base}/jeopardy/ss.jpg`
 	},
 	{
-		imgurl: '/jeopardy/hf.png'
+		imgurl: `/${base}/jeopardy/hf.png`
 	}
 ];
 
